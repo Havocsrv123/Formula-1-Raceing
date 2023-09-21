c97ed95f32d264875f6d613d25bc872c5a0c0dce
Havocsrv123 <81192475+Havocsrv123@users.noreply.github.com>
2023-09-20 19:47:35 -0600

    Example Files

---
+++ databrick course examples/Notebook introduction.py
@@ -0,0 +1,41 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Notebook Introduction
+# MAGIC ## UI Introduction
+# MAGIC ## Magic Commands
+# MAGIC - %python
+# MAGIC - %sql
+# MAGIC - %scala
+# MAGIC - %md
+
+# COMMAND ----------
+
+message = 'hello'
+
+# COMMAND ----------
+
+print(message)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT 'HELLO'
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC val msg = "hello"
+# MAGIC print(msg)
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
+
+# COMMAND ----------
+
